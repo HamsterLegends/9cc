48900299cc8355e3fd1ecaebc99ee94d4a342020
HamsterLegends <veryhotginger@gmail.com>
2025-07-20 12:31:01 +0900

    足し算と引き算の追加

--- 9cc.c
+++ 9cc.c
@@ -1,3 +1,4 @@
+//SPDX-License-Identifier: GPL-2.0-only
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,10 +9,29 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
+	char *p = argv[1];
+
 	printf(".intel_syntax noprefix\n");
 	printf(".globl main\n");
 	printf("main:\n");
-	printf("  mov rax, %d\n", atoi(argv[1]));
+	printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+	while(*p) {
+		if (*p == '+') {
+			p++;
+			printf("  add rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+		
+		if (*p == '-') {
+			p++;
+			printf("  sub rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+
+		fprintf(stderr,"予期しない文字です: '%c'\n", *p);
+		return 1;
+	}
 	printf("  ret\n");
 	printf(".section .note.GNU-stack,\"\",@progbits\n");
 	return 0;
